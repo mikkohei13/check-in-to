0fa465d2845b7397ece2084f1d0451b5c07505df
Mikko Heikkinen <hawk@biomi.org>
2017-01-26 09:00:11 +0200

    Init boilerplate

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Check-in</title>
+		<link rel="shortcut icon" href="favicon.ico">
+
+		<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+		<link rel="stylesheet" href="media/app.css" media="all" />
+
+	    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+	    <script src="node_modules/handlebars/dist/handlebars.min.js"></script>
+
+		</script>
+	</head>
+	<body>
+		<header>
+			<h1>Check-in to</h1>
+		</header>
+
+		<div id="content">
+
+		</div>
+
+		<script src="js/app.js"></script>
+	</body>
+</html>
