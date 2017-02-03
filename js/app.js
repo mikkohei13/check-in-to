fdd954f027033d23d125685c32f864e2ac9e058c
Mikko Heikkinen <hawk@biomi.org>
2017-02-03 20:29:19 +0200

    URL format with .htaccess

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+console.log(document.location);
+
+
+
+
+
+
+// From A-Frame VR toolkit
+// https://davidwalsh.name/query-string-javascript
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
